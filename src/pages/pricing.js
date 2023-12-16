0fea59464d072b0114bfe81a92d57fdad0c4174c
DamianoLuzi <luzi.dami03@gmail.com>
2023-12-16 21:27:41 +0100

    fixed logo

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -1,27 +1,7 @@
 export default function PricingPage() {
   return(
-    <div className = "section bg-pink">
-        <div className = "section bg-teal">
-            <h1>Title</h1>    
-      <ul>
-        <li>30</li>
-        <p>paragraph</p>
-        <li>30</li>
-        <li>30</li>
-        <li>30</li>
-      </ul>
-    </div>
-    <div className = "section bg-blue">
-      <ul>
-        <li>30</li>
-        <li>30</li>
-        <li>30</li>
-        <li>30</li>
-      </ul>
-    </div>
-    </div>
-    
-    
+  <>
+  </>
   );
 }
 
