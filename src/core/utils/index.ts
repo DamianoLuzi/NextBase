36dfd354ceb4e219bf16fb583c2dd8320dfdb1b1
DamianoLuzi <luzi.dami03@gmail.com>
2024-03-11 21:48:16 +0100

    modified production url config

--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -1,3 +1,3 @@
 const production = process.env.NODE_ENV === "production";
 //reusable site url (dependent on environment) for making request to API routes
-export const SITE_URL = production ? "https://next-supabase-saas.vercel.app/" : "http://localhost:3000";
+export const SITE_URL = production ? "https://next-supabase-saas.vercel.app/" /* : "http://localhost:3000" */;
